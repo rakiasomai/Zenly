d0a52b5bdfbd0ad1c7d599ad282483c618d28a0d
rakiasomai <somai.rakia@hotmail.fr>
2021-03-11 13:47:35 +0100

    fix

---
+++ client/client.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+
+	pb "github.com/rakiasomai/Zenly/proto"
+
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+var conn grpc.ClientConnInterface
+var err error
+
+
+// start user add function
+func user_add() {
+	client := pb.NewKVClient(conn)
+	var k string
+	var v string
+
+	fmt.Printf("Write The key : ")
+	fmt.Scanf("%s", &k)
+
+	fmt.Printf("Write The value : ")
+	fmt.Scanf("%s", &v)
+	
+	in := &pb.KeyValue{Key: k, Value: v}
+	stream, err := client.Add(context.Background(), in)
+	if err != nil {
+		log.Fatalf("openn stream error %v", err)
+	}
+
+	//ctx := stream.Context()
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true //close(done)
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+			log.Printf("Resp received: %s", resp.Value)
+		}
+	}()
+	<-done
+	log.Printf("finished")
+}
+
+// start user get function 
+func user_get() {
+	client := pb.NewKVClient(conn)
+	var k string
+
+	fmt.Printf("Write The key that you want to get : ")
+	fmt.Scanf("%s", &k)
+
+	
+	in := &pb.KeyRequest{Key: k}
+	stream, err := client.Get(context.Background(), in)
+	if err != nil {
+		log.Fatalf("openn stream error %v", err)
+	}
+
+	//ctx := stream.Context()
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true //close(done)
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+			log.Printf("Resp received: %s", resp.Value)
+		}
+	}()
+	<-done
+	log.Printf("finished")
+}
+
+
+
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	// dail server
+	conn, err = grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("can not connect with server %v", err)
+	}
+
+	for {
+		 user_add()
+		user_get()
+	}
+
+
+}
--- server/server.go
+++ server/server.go
@@ -5,13 +5,47 @@ import (
 	"log"
 	"net"
 	"sync"
+	"strconv"
 
 	pb "github.com/rakiasomai/Zenly/proto"
 
 	"google.golang.org/grpc"
 )
 
+var ErrNotfound = fmt.Errorf("name nor found")
+
 type server struct{}
+
+
+type Ob struct {
+	Key string
+	Value string
+}
+
+/* type Obs []*Ob */
+
+var ObList = []*Ob{
+	&Ob{
+		Key: "Khalil",
+		Value: "Ariana",
+	},
+	&Ob{
+		Key: "Rakia",
+		Value: "Tunis",
+	},
+}
+
+
+func findAdd(name string) (*Ob, error) {
+	i, err := strconv.Atoi(find(name)) 
+	if err == nil {
+		return nil, ErrNotfound
+	}
+	if i == -1 {
+		return nil, ErrNotfound
+	}
+	return ObList[i], nil
+}
 
 func (s server) Add(in *pb.KeyValue, srv pb.KV_AddServer) error {
 	log.Printf("fetch response for key : %d", in.Key)
@@ -19,9 +53,24 @@ func (s server) Add(in *pb.KeyValue, srv pb.KV_AddServer) error {
 	for i := 0; i < 1; i++ {
 		wg.Add(1)
 
-		go func(count int64) {
+		log.Printf("%s  %s", in.Key, in.Value)
 
-			resp := pb.Response{Result: fmt.Sprintf("Request #%d For Id:%d", count, in.Key)}
+		newKV :=  Ob{Key: in.Key, Value: in.Value}
+
+		ObList = append(ObList, &newKV)
+		log.Printf("%v", ObList)
+
+		/* newList = append(ObList, {Key: in.Key, Value: in.Value}) */
+		/* &ObList = append(&ObList, {Key: in.Key, Value: in.Value}) */
+		/* ObList = append(ObList,)
+		log.Printf("%v", ObList)
+		log.Printf("%v", newList) */
+
+		
+
+		go func(count int64) {
+			/* Value := "Tunis" */
+			resp := pb.KeyValue{Value: fmt.Sprintf("Request #%d : your adress is : %s", count, in.Value)}
 			if err := srv.Send(&resp); err != nil {
 				log.Printf("send error %v", err)
 			}
@@ -32,7 +81,55 @@ func (s server) Add(in *pb.KeyValue, srv pb.KV_AddServer) error {
 	return nil
 }
 
+func find(name string) string {
+	for i, p := range ObList {
+		if p.Key == name {
+			return  strconv.Itoa(i) 
+		}
+	}
+	return  strconv.Itoa(-1)
 }
+
+func getValueByKey(name string) string {
+	for _, p := range ObList {
+		if p.Key == name {
+			return p.Value
+		}
+	}
+	return ""
+}
+
+func (s server) Get(in *pb.KeyRequest, srv pb.KV_GetServer) error {
+	log.Printf("fetch response for key : %d", in.Key)
+	var wg sync.WaitGroup
+	for i := 0; i < 1; i++ {
+		wg.Add(1)
+
+		userKey := in.Key
+
+		if find(userKey) != "-1" {
+			value := getValueByKey(userKey)
+			log.Printf("The value of %s is %s", userKey, value)
+		} else {
+			log.Printf("The %s does not exist", userKey)
+		}
+		go func(count int64) {
+
+			resp := pb.KeyResponse{Value: fmt.Sprintf("Request #%d For name: %s", count, in.Key)}
+			if err := srv.Send(&resp); err != nil {
+				log.Printf("send error %v", err)
+			}
+			log.Printf("finishing request number : %d", count)
+		}(int64(i))
+	}
+	wg.Wait()
+	return nil
+}
+
+
+
+
+
 
 func main() {
 	// create listiner
@@ -43,7 +140,7 @@ func main() {
 
 	// create grpc server
 	s := grpc.NewServer()
-	pb.RegisterStreamServiceServer(s, server{})
+	pb.RegisterKVServer(s, server{})
 
 	log.Println("start server")
 	// and start...
